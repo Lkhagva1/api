9f30c2afc4b6fb48ccb527f51f545fe0073487c0
Lkhagva1 <b.lhagvaa2000@gmail.com>
2023-04-21 17:11:35 +0800

    ds

--- models/user.js
+++ models/user.js
@@ -1,47 +1,46 @@
-const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema.Types
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 const userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true
-    },
-    lastname:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    branch:{
-        type:String,
-        required:true
-    },
-    isAdmin:{
-        type:Boolean,
-        default:false
-    },
-    city:{
-        type:String,
-        required:true
-    },
-    mobile:{
-        type:String,
-        required:true
-    },
-    stateName:{
-        type:String,
-        required:true
-    },
-    pic:{
-     type:String,
-     default:"https://res.cloudinary.com/cnq/image/upload/v1586197723/noimage_d4ipmd.png"
-    }
-})
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  stateName: {
+    type: String,
+    required: true,
+  },
+  pic: {
+    type: String,
+    default: "https://icon-library.com/images/avater-icon/avater-icon-7.jpg",
+  },
+});
 
-
-mongoose.model("User",userSchema)
+mongoose.model("User", userSchema);
